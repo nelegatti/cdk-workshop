9b5b7688bff220017d9c3a72cb1cc05019aedbb3
nelegatti <nelegatti.93@gmail.com>
2021-07-26 17:18:22 -0300

    apigateway created, backed by lambda function

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,14 +1,21 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // define Lambda resource 
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler'
+    });
+
+    // define an Api Gateway Rest Api resource backed by "hello" function
+    new apigateway.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
   }
 }
