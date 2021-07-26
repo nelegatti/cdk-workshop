46edbde261f015a39c14c7558019dfb8ba2cd4b1
nelegatti <nelegatti.93@gmail.com>
2021-07-26 17:10:04 -0300

    Deploy Lambda function

---
+++ lambda/hello.js
@@ -0,0 +1,9 @@
+exports.handler = async event => {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello, CDK! You have hit ${event.path}\n`
+  }
+}
