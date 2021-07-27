975b0266e8e6b293d6671f7acb7456937802883d
nelegatti <nelegatti.93@gmail.com>
2021-07-27 14:13:43 -0300

    Order table viewer by descending hits order

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -26,7 +26,8 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
-      table: helloWithCounter.table
+      table: helloWithCounter.table,
+      sortBy: '-hits'
     })
   }
 }
